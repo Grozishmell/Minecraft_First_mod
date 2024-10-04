4794b3144688f715bbdc10f98edf5c5444cf392e
Grozishmell <grozamarshmellow@gmail.com>
2024-10-04 10:16:51 +0300

    Version 0.2

--- src/main/java/net/grozishmell/firstmod/FirstMod.java
+++ src/main/java/net/grozishmell/firstmod/FirstMod.java
@@ -1,6 +1,10 @@
 package net.grozishmell.firstmod;
 
 import com.mojang.logging.LogUtils;
+import net.grozishmell.firstmod.item.ModCreativeModTabs;
+import net.grozishmell.firstmod.item.ModItems;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
@@ -27,6 +31,10 @@ public class FirstMod
     {
         IEventBus modEventBus = context.getModEventBus();
 
+        ModCreativeModTabs.register(modEventBus);
+
+        ModItems.register((modEventBus));
+
         modEventBus.addListener(this::commonSetup);
 
         MinecraftForge.EVENT_BUS.register(this);
@@ -41,7 +49,10 @@ public class FirstMod
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event)
     {
-
+        if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.SAPPHIRE);
+            event.accept(ModItems.RAW_SAPPHIRE);
+        }
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
